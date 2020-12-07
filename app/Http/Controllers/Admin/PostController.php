6a464a307c5dfa19565b08f823ba616373a1f2ec
Valerio <valerio.modesti@gmail.com>
2020-12-07 10:00:03 +0100

    create

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        // prendo i dati dal form in forma di array col metodo all()
+        $data = $request->all();
+        $request->validate([
+            'title' => 'required|min:5|max:100',
+            'body' => 'required|min:5|max:500',
+            'img' => 'image'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        {{-- enctype aggiunto per img, serve per inviare dati che non siano testo --}}
+        <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+            {{-- Laravel automatically generates a CSRF "token" for each active user session managed by the application.
+            This token is used to verify that the authenticated user is the one actually making the requests to the application. --}}
+            @csrf
+            @method('POST')
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            <div class="form-group">
+                <label for="title">Titolo</label>
+                <input input="text" name="title" class="form-control" placeholder="Inserisci il titolo">
+            </div>
+            <div class="form-group">
+                <label for="img">Immagine</label>
+                <input type="file" name="img" accept="image/*">
+            </div>
+            <div class="form-group">
+                <label for="body">Body</label>
+                <textarea class="form-control" name="body" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+
+@endsection
