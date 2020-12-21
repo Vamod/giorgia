11002f3e258ea73eecd833620902fe6d21787aa9
Valerio <valerio.modesti@gmail.com>
2020-12-21 20:20:28 +0100

    chisono

--- resources/views/guests/chisono.blade.php
+++ resources/views/guests/chisono.blade.php
@@ -4,20 +4,17 @@
     <div class="container">
         <div class="row">
             {{-- offset-lg-2  --}}
-            <div class="col-lg-8">
-                <div class="img-chi-sono">
+            <div class="offset-lg-2 col-lg-8">
+                <div class="img-chi-sono text-center">
                     <img src="{{ asset('img/giorgia-cesarini.jpg') }}" alt="GiorgiaCesarini">
                 </div>
-                <h2 class="title-chi-sono">CHI SONO</h2>
+                <h2 class="title-chi-sono text-center">CHI SONO</h2>
                 <p class="chi-sono-body">Sono la <strong>dott.ssa Giorgia Cesarini</strong>, psicologa iscritta all'albo con matricola 22047 e psicoterapeuta ad orientamento <strong>Sistemico - Relazionale</strong> (in formazione). <br>
                     Laureata presso l'Università La Sapienza di Roma in Psicologia dello Sviluppo, dell'Educazione e del Benessere.
                     Specializzata nel trattamento dei Disturbi dello Spettro Autistico, ADHD e Disabilità psichica. Offro consulenza psicologica a famiglie e coppie genitoriali attraverso Parent Training e interventi di sostegno alla genitorialità.
                     Esperta in Disturbi Specifici dell'Apprendimento e formata come Tutor allo Studio, svolgo interventi specialistici di training e potenziamento per bambini e ragazzi con DSA.
                     Per tutti i miei interventi utilizzo un approccio sistemico - relazionale, considerando il singolo individuo imprescindibilmente legato ai suoi contesti relazionali e al suo sistema familiare.
                     L'ottica sistemica prevede dunque una presa in carico globale della persona, in cui la famiglia possiede un ruolo attivo e partecipe, finalizzato al raggiungimento del cambiamento desiderato.</p>
-            </div>
-            <div class="col-lg-4 bkg-img">
-                {{-- <img src="{{ asset('img/tree.png') }}" alt="tree"> --}}
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,9 @@
                     <div class="nav-title">Dott.ssa <span class="title-name">Giorgia Cesarini</span></div>
                     <div class="nav-descr">Psicologa - Psicoterapeuta</div>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}"> --}}
                     {{-- <span class="navbar-toggler-icon"></span> --}}
-                </button>
+                {{-- </button> --}}
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
